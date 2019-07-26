36642848c2deb28f91aa4a8f566dfe74ea411f0f
Jeremy Yandell <jereyand@gmail.com>
2019-07-26 10:14:09 -0700

    setup server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express        = require('express');
+const bodyParser     = require('body-parser');
 const methodOverride = require('method-override');
+const session        = require('express-session');
+const app            = express();
 
+require('./db/db');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+app.listen(3000, () => {
+    console.log('listening..... on port 3000');
+  });
 
