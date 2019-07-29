d1ce83471f3dc67076daf1999a2a932e7ba2ba32
Narek <narex111@gmail.com>
2019-07-29 10:43:56 -0700

    drop downs working

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,11 +27,6 @@ const userController = {
     showUser: async (req,res,next)  =>  {
         try  {
             const foundUser = await Users.findById(req.params.id).populate('topTenMovies')
-            console.log(foundUser, 'from show page ---- should be populated');
-            // const foundUser = await Users.findById(req.params.id);
-            // const foundMovies = await Users.findById(req.params.id).populate('topTenMovies');
-            // console.log(foundMovies)
-            // console.log(foundUser);
             res.render('users/show.ejs', {
                 user: foundUser
             });
@@ -53,11 +48,18 @@ const userController = {
     },
     updateTopTen: async (req,res,next)  =>  {
         try  {
-            console.log(req.body);
             const foundUser = await Users.findById(req.params.id);
-            const movieId = await Movies.findById(req.body.movieId);
-            await foundUser.topTenMovies.push(movieId);
+            for(i=foundUser.topTenMovies.length; i>0; i--){
+                foundUser.topTenMovies.pop()
+            }
+
+            for (i=0; i<3; i++){
+                let movieId = await Movies.findById(req.body[i]);
+                console.log(movieId, "in for loop")
+                await foundUser.topTenMovies.push(movieId);
+            }
             await foundUser.save()
+            console.log(foundUser, "<--user after an update")
             res.redirect("/users/"+foundUser._id)
         }  catch(err)  {
             next(err)
