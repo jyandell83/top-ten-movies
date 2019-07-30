62acca68ce2940793285665eeef7c8ea2b88a59e
Narek <narex111@gmail.com>
2019-07-30 16:54:18 -0700

    some work on button

--- models/Users.js
+++ models/Users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     userName: {type: String, required: true, unique:true},
     password: {type: String, required: true},
+    likes: Number,
     topTenMovies: [{
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Movie'
